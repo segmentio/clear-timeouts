da9add1f641ed3d796657f1131b95e6c19a054a7
Nathan Houle <nathan+github@nathanhoule.com>
2016-05-26 21:04:02 -0700

    Modernize test harness; remove Duo support, add Browserify support (#1)

---
+++ README.md
@@ -0,0 +1,18 @@
+# clear-timeouts
+
+[![CircleCI](https://circleci.com/gh/segmentio/clear-timeouts.svg?style=shield&circle-token=5c2ac0ba06015f54d3e5c7d688c973210eb18fc2)](https://circleci.com/gh/segmentio/clear-timeouts)
+[![Codecov](https://img.shields.io/codecov/c/github/segmentio/clear-timeouts/master.svg?maxAge=2592000)](https://codecov.io/gh/segmentio/clear-timeouts)
+
+clear all timeouts
+
+## Installation
+
+```sh
+$ npm install @segment/clear-timeouts
+```
+
+## API
+
+#### clear()
+
+Clear all timeouts
---
+++ test/index.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var assert = require('proclaim');
+var clear = require('../lib');
+
+describe('clear-timeouts', function() {
+  var j = 0;
+  function incr() { ++j; }
+  setTimeout(incr);
+  setTimeout(incr, 50);
+  setTimeout(incr, 90);
+  setTimeout(incr, 100);
+
+  it('should clear timeouts', function(done) {
+    setTimeout(function() {
+      clear();
+      assert(j === 2);
+      done();
+    }, 51);
+  });
+
+  it('should clear', function(done) {
+    setTimeout(incr, 50);
+    setTimeout(function() {
+      clear();
+      assert(j === 3);
+      done();
+    }, 101);
+  });
+});
